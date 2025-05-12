77678aa2f6d1e70a46577ee1e599584ae53172ce
AhmedovBaloglan <baloglanhmdov05@gmail.com>
2025-05-12 22:37:45 +0400

    envato market

--- envatoma.js
+++ envatoma.js
@@ -1,12 +1,11 @@
-const toggleBtn = document.getElementById('toggleBtn');
-const hiddenRow = document.querySelector('.gridCard1');
-
-toggleBtn.addEventListener('click', () => {
-  hiddenRow.classList.toggle('visible');
-
-  if (hiddenRow.classList.contains('visible')) {
-    toggleBtn.textContent = 'Daha Az Göstər';
-  } else {
-    toggleBtn.textContent = 'Daha Çox Göstər';
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".buttonslayd");
+  button.addEventListener("click", function () {
+    const hiddenImages = document.querySelectorAll(".gridCard.hidden");
+    hiddenImages.forEach(image => {
+      image.classList.remove("hidden");
+      image.classList.add("visible");
+    });
+    this.style.display = "none";
+  });
 });
